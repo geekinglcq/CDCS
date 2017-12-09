4620afedc9f7b38453647a595b3313e90660caec
lunar <lcqgeek@live.com>
2017-12-09 21:31:47 +0800

    add img files

--- utility.py
+++ utility.py
@@ -1,10 +1,10 @@
 # -*- coding:utf-8 -*-
 import json
-
+import codecs 
 from pprint import pprint 
 
 def addSolutionInfo(id):
-    with open('solutions2.json', 'r') as f:
+    with codecs.open('solutions2.json', 'r', encoding='utf-8') as f:
         solutions = json.load(f)
     if str(id) in solutions.keys():
         print("ID exsited, please try again")
@@ -13,18 +13,18 @@ def addSolutionInfo(id):
     code = input("Enter code: ")
     s = {"algorithm": algo, "code":code}
     solutions[id] = s
-    with open("solution2.json", 'w') as f:
+    with codecs.open("solution2.json", 'w', encoding='utf-8') as f:
         json.dump(solutions, f, indent=4)
 
 def addCompe():
-    with open('competitions2.json', 'r') as f:
+    with codecs.open('competitions2.json', 'r', encoding='utf-8') as f:
         coms = json.load(f)
-    with open('solutions2.json', 'r') as f:
+    with codecs.open('solutions2.json', 'r', encoding='utf-8') as f:
         solutions = json.load(f)
     cid = max([int(i) for i in solutions.keys()]) + 1
     newCom = {}
     for i in ["name", "link", "type", "platform", "pic", "hosts", "ddl"]:
-        newCom[i] = input("Enter %d"%(i))
+        newCom[i] = input("Enter %s"%(i))
     solution = []
     num = input("How many solutins you find?  ")
     for i in range(num):
@@ -36,5 +36,5 @@ def addCompe():
         
     newCom["solution"] = solution
     coms.append(newCom)
-    with open("solutions2.json", "w") as f:
-        json.dump(f, coms)
+    with codecs.open("solutions2.json", "w", encoding='utf-8') as f:
+        json.dump(f, coms, indent=4)

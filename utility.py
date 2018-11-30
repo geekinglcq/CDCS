bb5372d706c181cf00d2337cc8a0120d30842874
lunar <lcqgeek@live.com>
2018-11-30 23:14:29 +0800

    :sparkles: add 3 tianchi comp

--- utility.py
+++ utility.py
@@ -1,7 +1,8 @@
 # -*- coding:utf-8 -*-
 import json
-import codecs 
-from pprint import pprint 
+import codecs
+from pprint import pprint
+
 
 def addSolutionInfo(id):
     """
@@ -11,13 +12,14 @@ def addSolutionInfo(id):
         solutions = json.load(f)
     if str(id) in solutions.keys():
         print("ID exsited, please try again")
-        return 
+        return
     algo = input("Enter algorithm: ")
     code = input("Enter code: ")
-    s = {"algorithm": algo, "code":code}
+    s = {"algorithm": algo, "code": code}
     solutions[id] = s
     with codecs.open("solutions.json", 'w', encoding='utf-8') as f:
         json.dump(solutions, f, indent=4)
+
 
 def addCompe():
     """
@@ -29,22 +31,22 @@ def addCompe():
         solutions = json.load(f)
     cid = max([int(i) for i in solutions.keys()]) + 1
     newCom = {}
-    for i in ["name", "link", "type", "platform", "pic", "hosts", "ddl"]:
-        newCom[i] = input("Enter %s: "%(i))
-    newCom["pic"] = "https://github.com/geekinglcq/CDCS/blob/master/img/%s?raw=true"%(newCom["pic"])
+    for i in ["name", "link", "type", "platform", "hosts", "ddl"]:
+        newCom[i] = input("Enter %s: " % (i))
     solution = []
     num = input("How many solutins you find?  ")
     for i in range(int(num)):
         rank = input("Rank: ")
-        print("The id of this solutions is %d"%(cid))
+        print("The id of this solutions is %d" % (cid))
         addSolutionInfo(cid)
-        solution.append({"rank":rank, "id":cid})
+        solution.append({"rank": rank, "id": cid})
         cid += 1
-        
+
     newCom["solutions"] = solution
     coms.append(newCom)
     with codecs.open("competitions.json", "w", encoding='utf-8') as f:
         json.dump(coms, f, indent=4)
+
 
 def renderToMK():
     data = codecs.open("header.md", 'r', 'utf-8').readlines()
@@ -56,20 +58,19 @@ def renderToMK():
     f.write("||名称|类型|截止日期|解决方案|平台|主办方|  \n")
     f.write("|--|--|--|--|--|--|--|  \n")
     for com in coms:
-        f.write("|![](http://p0qqn3m6c.bkt.clouddn.com/%s?imageView2/2/w/100/h/100/q/75)|[%s](%s)|%s|%s|"%( \
-             com['pic'][51:-9], com['name'], com['link'], com['type'], com['ddl']))
+        f.write("|[%s](%s)|%s|%s|"%( \
+              com['name'], com['link'], com['type'], com['ddl']))
         soStrings = []
         print(com['name'])
         for solution in com['solutions']:
-            st = "第%s名 "%(solution['rank'])
+            st = "第%s名 " % (solution['rank'])
             so = solutions[str(solution['id'])]
             if so['algorithm'] != "null":
-                st = "%s [算法](%s)"%(st, so['algorithm'])
+                st = "%s [算法](%s)" % (st, so['algorithm'])
             if so['code'] != "null":
-                st = "%s [代码](%s)"%(st, so['code'])
+                st = "%s [代码](%s)" % (st, so['code'])
             soStrings.append(st)
         f.write('<br>'.join(soStrings))
-        f.write("|%s|%s|  \n"%(com['platform'], com['hosts']))
-    
+        f.write("|%s|%s|  \n" % (com['platform'], com['hosts']))
+
     f.close()
-        
